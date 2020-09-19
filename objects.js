a2cc82fddd6981b6c8966019af5d9f5c852cf419
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-19 17:41:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
-var playlist = { [Harry Case]: "In A Mood", [Free Music]: "Pube's Lament", [Oariana]: "A Pear In The Wind"}
+var playlist = { Harry Case: "In A Mood", Free Music: "Pube's Lament", Oariana: "A Pear In The Wind"}
 
-function updatePlaylist {
-  
+function updatePlaylist (targetObject, updatesObject) {
+  return Object.assign({}, targetObject, updatesObject)
 }
 
 function removeFromPlaylist {
-
+  delete playlist.Oariana
 }
