5c71510836ae66aae5dd29cc9af3b3631e78316d
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-19 17:37:56 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { [Harry Case]: "In A Mood", [Free Music]: "Pube's Lament", [Oariana]: "A Pear In The Wind"}
+
+function updatePlaylist {
+  
+}
+
+function removeFromPlaylist {
+  
+}
