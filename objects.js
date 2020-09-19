d9441e4375b7b394e0f4b0b844dda5be6e0bfe02
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-19 17:48:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { harrycase: "In A Mood" }
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  return Object.assign({playlist}, {artistName}, {songTitle})
+  return Object.assign({artistName: songTitle})
 }
 
 function removeFromPlaylist (playlist, artistName) {
